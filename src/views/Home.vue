da9379da71dbdbfdb1d86da25a546474bd14850d
Taegon Kim <taegon_kim@brown.edu>
2021-08-16 06:14:12 +0900

    login, signup, logout, index, create actions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,67 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>{{ message }}</h1>
+
+    <div v-for="group in groups" v-bind:key="group.id">
+      <p>{{ group.subject }}</p>
+      <p>{{ group.zipcode }}</p>
+      <p>{{ group.user_id }}</p>
+      <p>{{ group.id }}</p>
+      <hr>
+    </div>
+    <div>
+      <p>
+        Subject: <input type="text" v-model="newGroupParams.subject" />
+      </p>
+      <p>
+        Zipcode: <input type="text" v-model="newGroupParams.zipcode" />
+      </p>
+      <p>
+        Maximum Number of Students: <input type="text" v-model="newGroupParams.max_students" />
+      </p>
+      <p>
+        Owner ID: <input type="text" v-model="newGroupParams.user_id" />
+      </p>
+      <button v-on:click="createGroup()">Create Group</button>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      groups: [],
+      newGroupParams: {},
+    };
+  },
+  created: function () {
+    // this.groupsIndex();
+  },
+  methods: {
+    groupsIndex: function () {
+      axios.get("/groups").then((response) => {
+        console.log("groups index", response);
+        this.groups = response.data;
+      });
+    },
+    createGroup: function () {
+      axios
+        .post("/groups", this.newGroupParams)
+        .then((response) => {
+          console.log("groups create", response);
+          this.groups.push(response.data);
+          this.newGroupParams = {};
+        })
+        .catch((error) => {
+          console.log("groups create error", error.response);
+        });
+    },
+  },
+};
 </script>
